5bec3adbbdd7c232afe373324ef1db2e6155005b
sakthi <sakthiratnam050@gmail.com>
2025-05-08 13:57:35 +0530

    🧪 Fixed test cases and made it dyanmic

--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -4,13 +4,39 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 func TestHeadersParse(t *testing.T) {
-	headers := Headers{}
-	_, _, err := headers.Parse([]byte("Host: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
-	require.NoError(t, err)
-	assert.Equal(t, "localhost:42069", headers["Host"])
+	testCases := []struct {
+		headerByte []byte
+		headerKey  string
+		headerVal  string
+		errString  string
+	}{
+		{[]byte("Host: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"),
+			"Host", "localhost:42069",
+			""}, // valid header test case
+		{[]byte("Host : localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"),
+			"Host", "localhost:42069",
+			"Not a valid header"}, // Invalid spacing header key
+		{[]byte("Host: localhost:42"),
+			"", "", ""},
 
+		{[]byte("Host: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"),
+			"Host", "localhost:42069", "",
+		},
+	}
+
+	headers := Headers{}
+	for _, testCase := range testCases {
+		_, _, err := headers.Parse(testCase.headerByte)
+		if err != nil {
+
+			assert.Equal(t, err.Error(), testCase.errString)
+			continue
+		}
+		if testCase.headerKey != "" {
+			assert.Equal(t, testCase.headerVal, headers[testCase.headerKey])
+		}
+	}
 }
